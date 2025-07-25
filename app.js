bcadf11cfab4e118b5e341749528c15fbe99782c
Caleb Bluesummers <calebbluesummers@gmail.com>
2025-07-24 17:54:47 -1000

    swapped filenames, add main.js

---
+++ app.js
@@ -0,0 +1,145 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+const fs = require('fs')
+const path = require('path')
+// import { Conf } from 'electron-conf/main'
+
+// const confPath = path.join(app.getPath('userData'), 'layouts.json')
+const confPath = path.join('layouts.json')
+
+let MainWin
+let overlayWins = new Array
+let config
+let activeLayout
+
+function readConfig() {
+	let contents = fs.readFileSync(confPath, 'utf8')
+	config = JSON.parse(contents)
+	// return JSON.parse(contents)
+}
+
+function parseConfig() {
+	if (Object.hasOwn(config, 'last_active')) {
+		if (config.last_active !== ""){
+			activeLayout = config.last_active
+		} else {
+			activeLayout = ""
+		}
+	}
+}
+
+function writeConfig() {
+	let contents = JSON.stringify(config, null, 4)
+	fs.writeFileSync(confPath, contents)
+}
+
+const createMain = () => {
+	MainWin = new BrowserWindow({
+		show: false,
+		autoHideMenuBar: true,
+		webPreferences: {
+			preload: path.join(__dirname, 'preload.js'),
+			contextIsolation: true,
+			nodeIntegrationInWorker: true
+		}
+	})
+
+	// MainWin.webContents.openDevTools()
+	MainWin.loadFile('main.html');
+	MainWin.show();
+	
+	MainWin.on('closed', () => app.quit())
+}
+
+const createOverlay = () => {
+	let overlayWin = new BrowserWindow({
+		frame: false,
+		autoHideMenubar: true,
+		transparent: true,
+		alwaysOnTop: true,
+		show: false,
+		webPreferences: {
+			preload: path.join(__dirname, 'preload.js'),
+			contextIsolation: true,
+			nodeIntegrationInWorker: true
+		}
+	})
+
+	overlayWin.setFullScreen(true);
+	overlayWin.setIgnoreMouseEvents(true);
+	// overlayWin.webContents.openDevTools()
+	overlayWin.loadFile('overlay.html');
+	overlayWin.show();
+	overlayWins.push(overlayWin)
+}
+
+app.whenReady().then(() => {
+	readConfig()
+	parseConfig()
+	createMain();
+	
+	MainWin.webContents.on('before-input-event', (event, input) => {
+		if (input.control && input.key.toLowerCase() === 'r') {
+			MainWin.webContents.reloadIgnoringCache()
+			event.preventDefault()
+		}
+	})
+
+	app.on('activate', () => {
+		if (BrowserWindow.getAllWindows().length === 0) {
+			createMain();
+		}
+	})
+})
+
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+})
+
+ipcMain.on("toMain", (event, args) => {
+	console.log("toMain args = " + args);
+	let cmd = args[0]
+	if (cmd == "readConfig") {
+		console.log(config.layouts[activeLayout])
+		overlayWins[0].webContents.send("fromMain", config.layouts[activeLayout])
+	}
+	if (cmd == "requestConfig") {
+		MainWin.webContents.send("configReturn", config)
+	}
+	if (cmd == "updateActive") {
+		config.last_active = args[1]
+		writeConfig()
+		readConfig()
+		parseConfig()
+	}
+	if (cmd == "refresh") {
+	  MainWin.webContents.reloadIgnoringCache()
+	  event.preventDefault()
+	}
+	if (cmd == "activate") {
+		let name = args[1]
+		let newValues = args[2]
+		console.log(config.layouts[name])
+		config.layouts[name] = newValues
+		console.log(config.layouts[name])
+
+		if (overlayWins[0]) {
+			overlayWins[0].show()
+			overlayWins[0].webContents.send("fromMain", config.layouts[activeLayout])
+		}
+		else {
+			createOverlay()
+		}
+	}
+	if (cmd == "save") {
+		writeConfig()
+	}
+	if (cmd == "deactivate") {
+		if (overlayWins[0]) {
+			overlayWins[0].close()
+			overlayWins.splice(0, 1)
+		}
+	}
+})
--- main.js
+++ main.js
@@ -1,145 +1,49 @@
-const { app, BrowserWindow, ipcMain } = require('electron')
-const fs = require('fs')
-const path = require('path')
-// import { Conf } from 'electron-conf/main'
+let layouts
+window.bridge.send("toMain", ["requestConfig"]);
+window.bridge.receive("configReturn", (data) => {
+    console.log(data);
+    let activeLayout = ""
+    if (Object.hasOwn(data, 'last_active')) {
+        if (data.last_active !== ""){
+            activeLayout = data.last_active
+        }
+    }
 
-// const confPath = path.join(app.getPath('userData'), 'layouts.json')
-const confPath = path.join('layouts.json')
+    layouts = data.layouts
+    console.log(layouts)
+    // populate the selection
+    for (let key in layouts) {
+        let opt = document.createElement('option');
+        opt.value = key;
+        opt.innerHTML = key;
+        if (key == activeLayout) {
+            opt.selected = true
+            selectLayout(key)
+        }
+        document.getElementById('selectLayouts').append(opt);
+    }
 
-let MainWin
-let overlayWins = new Array
-let config
-let activeLayout
-
-function readConfig() {
-	let contents = fs.readFileSync(confPath, 'utf8')
-	config = JSON.parse(contents)
-	// return JSON.parse(contents)
+});
+function selectLayout(selectedLayout) {
+    document.getElementById('inputWidth').value = layouts[selectedLayout].width;
+    document.getElementById('inputHeight').value = layouts[selectedLayout].height;
+    document.getElementById('inputLeftOffset').value = layouts[selectedLayout].offsetLeft;
+    document.getElementById('inputBottomOffest').value = layouts[selectedLayout].offsetBottom;
+    window.bridge.send("toMain", ["updateActive", selectedLayout])
 }
+function sendUpdate() {
+    let sel = document.getElementById('selectLayouts')
+    let selected = sel.options[sel.selectedIndex].text
+    let layout = layouts[selected]
+    layout.width = document.getElementById('inputWidth').value
+    layout.height = document.getElementById('inputHeight').value
+    layout.offsetLeft = document.getElementById('inputLeftOffset').value
+    layout.offsetBottom = document.getElementById('inputBottomOffest').value
+    window.bridge.send('toMain', ['activate', selected, layout])
 
-function parseConfig() {
-	if (Object.hasOwn(config, 'last_active')) {
-		if (config.last_active !== ""){
-			activeLayout = config.last_active
-		} else {
-			activeLayout = ""
-		}
-	}
 }
-
-function writeConfig() {
-	let contents = JSON.stringify(config, null, 4)
-	fs.writeFileSync(confPath, contents)
+function saveToFile() {
+    let sel = document.getElementById('selectLayouts')
+    let selected = sel.options[sel.selectedIndex].text
+    window.bridge.send('toMain', ['save', selected])
 }
-
-const createMain = () => {
-	MainWin = new BrowserWindow({
-		show: false,
-		autoHideMenuBar: true,
-		webPreferences: {
-			preload: path.join(__dirname, 'preload.js'),
-			contextIsolation: true,
-			nodeIntegrationInWorker: true
-		}
-	})
-
-	// MainWin.webContents.openDevTools()
-	MainWin.loadFile('app.html');
-	MainWin.show();
-	
-	MainWin.on('closed', () => app.quit())
-}
-
-const createOverlay = () => {
-	let overlayWin = new BrowserWindow({
-		frame: false,
-		autoHideMenubar: true,
-		transparent: true,
-		alwaysOnTop: true,
-		show: false,
-		webPreferences: {
-			preload: path.join(__dirname, 'preload.js'),
-			contextIsolation: true,
-			nodeIntegrationInWorker: true
-		}
-	})
-
-	overlayWin.setFullScreen(true);
-	// overlayWin.setIgnoreMouseEvents(true);
-	overlayWin.webContents.openDevTools()
-	overlayWin.loadFile('overlay.html');
-	overlayWin.show();
-	overlayWins.push(overlayWin)
-}
-
-app.whenReady().then(() => {
-	readConfig()
-	parseConfig()
-	createMain();
-	
-	MainWin.webContents.on('before-input-event', (event, input) => {
-		if (input.control && input.key.toLowerCase() === 'r') {
-			MainWin.webContents.reloadIgnoringCache()
-			event.preventDefault()
-		}
-	})
-
-	app.on('activate', () => {
-		if (BrowserWindow.getAllWindows().length === 0) {
-			createMain();
-		}
-	})
-})
-
-
-app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') {
-		app.quit();
-	}
-})
-
-ipcMain.on("toMain", (event, args) => {
-	console.log("toMain args = " + args);
-	let cmd = args[0]
-	if (cmd == "readConfig") {
-		console.log(config.layouts[activeLayout])
-		overlayWins[0].webContents.send("fromMain", config.layouts[activeLayout])
-	}
-	if (cmd == "requestConfig") {
-		MainWin.webContents.send("configReturn", config)
-	}
-	if (cmd == "updateActive") {
-		config.last_active = args[1]
-		writeConfig()
-		readConfig()
-		parseConfig()
-	}
-	if (cmd == "refresh") {
-	  MainWin.webContents.reloadIgnoringCache()
-	  event.preventDefault()
-	}
-	if (cmd == "activate") {
-		let name = args[1]
-		let newValues = args[2]
-		console.log(config.layouts[name])
-		config.layouts[name] = newValues
-		console.log(config.layouts[name])
-
-		if (overlayWins[0]) {
-			overlayWins[0].show()
-			overlayWins[0].webContents.send("fromMain", config.layouts[activeLayout])
-		}
-		else {
-			createOverlay()
-		}
-	}
-	if (cmd == "save") {
-		writeConfig()
-	}
-	if (cmd == "deactivate") {
-		if (overlayWins[0]) {
-			overlayWins[0].close()
-			overlayWins.splice(0, 1)
-		}
-	}
-})
